eb62508dc8c661cbad1930ffd8160bb6f22d4bb1
umg <umg@HUE-LAB-1F1.labhue.local>
2026-04-25 09:48:03 -0600

    Agregué la Clase Biblioteca para la parte del Servicio

---
+++ diego_0904_25_19857/src/main/java/com/christopher_0904_25_19857/gestionbiblioteca/Diego_0904_25_19857.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.christopher_0904_25_19857.gestionbiblioteca;
+
+/**
+ *
+ * @author umg
+ */
+public class Diego_0904_25_19857 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
