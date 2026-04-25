8dc7ab0f7b865febc4c80b9388df8f1457220f33
umg <umg@HUE-LAB-1F1.labhue.local>
2026-04-25 10:10:07 -0600

    Agregué la Clase main para probar la aplicación

---
+++ diego_0904_25_19857/src/main/java/com/christopher_0904_25_19857/gestionbiblioteca/app/main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.christopher_0904_25_19857.gestionbiblioteca.app;
+
+/**
+ *
+ * @author umg
+ */
+public class main {
+    
+}
